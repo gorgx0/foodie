eaf9ed4f4d2a0ae3ddcf488cd44446908a75768e
Marcin Krasowski <marcin.krasowski@gmail.com>
2017-03-05 20:28:22 +0100

    added restaurant.id

--- model/src/main/java/com/foodie/model/Restaurant.java
+++ model/src/main/java/com/foodie/model/Restaurant.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class Restaurant {
 
+    private Long id;
     private String name ;
     private Boolean active ;
     private BigDecimal deliveryFee ;
