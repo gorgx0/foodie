cc13e7f1b7c65c881955d1cbd1f90b1745bdf0d3
Marcin Krasowski <marcin.krasowski@gmail.com>
2017-04-15 14:20:26 +0200

    invitation manager with

--- server/src/main/java/com/foodie/auth/UserSettingFilter.java
+++ server/src/main/java/com/foodie/auth/UserSettingFilter.java
@@ -43,31 +43,6 @@ public class UserSettingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        Cookie[] cookies = req.getCookies();
-        if (cookies == null) {
-            cookies = new Cookie[0];
-        }
-        Map<String, Cookie> cookieNamesMap = Stream.of(cookies).collect(Collectors.toMap(Cookie::getName, Function.identity()));
-        User u;
-        if(!cookieNamesMap.containsKey(USER_COOKIE_NAME)){
-            Cookie userCookie = new Cookie(USER_COOKIE_NAME, "cookieRandomValue");
-            res.addCookie(userCookie);
-            u = new User();
-            userGroupService.setUser(userCookie);
-        }else {
-            u = userGroupService.getUser(cookieNamesMap.get(USER_COOKIE_NAME));
-        }
-        if(!cookieNamesMap.containsKey(GROUP_COOKIE_NAME)){
-            Cookie groupCookie = new Cookie(GROUP_COOKIE_NAME, "cookieRandomValue");
-            res.addCookie(groupCookie);
-            Group lastGroup = u.getLastGroup();
-            if (lastGroup == null) {
-                Group group = new Group();
-                u.setLastGroup(group);
-            }
-        }
         chain.doFilter(request,response);
     }
 
